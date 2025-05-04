1feb3d443cf789ccfd83bf9394a2b8bbdc033346
Aswin-Hariram <aswincseskct@gmail.com>
2025-05-04 15:58:15 +0530

    prod 3

--- Backend/app.py
+++ Backend/app.py
@@ -176,5 +176,6 @@ def hello():
     }), 200
 
 # No debug mode here
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to 8001 if not set
+    app.run(host="0.0.0.0", port=port)
